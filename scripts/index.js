41e1b6cf27aea5e54a4de344904e9fb0bc43ba18
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2021-01-13 22:36:05 +0530

    completed

--- scripts/index.js
+++ scripts/index.js
@@ -19,25 +19,45 @@ const callMeSupreme = () => {
     }
 
     /* Main Game Function */
-
     const mainGame = () => {
         const rps = document.querySelectorAll('.circle');
-        const scoreElement = document.getElementById('score');
-        const winner = document.querySelector('.winner');
-        const mainGame = document.querySelector('.main-game');
 
-        const computer = ['rock', 'paper', 'scissors'];
+        const scoreElement = document.getElementById('score');
+
+        const mainGame = document.querySelector('.main-game');
+        const result = document.querySelector('.result');
+
+        const winner = document.querySelector('.heading-winner');
+
+        const resetBtn = document.querySelector('.reset');
+
+        const playerChoose = document.querySelector('.player-choose');
+        const computerChoose = document.querySelector('.computer-choose');
+        const announce = document.querySelector('.announcement');
+
+        const playerContainer = document.querySelector('.player-picked-container');
+        const computerContainer = document.querySelector('.house-picked-container');
+
+
+        const computer = ['paper', 'rock', 'scissors'];
         let score = 0;
+        scoreElement.textContent = 0;
 
         rps.forEach((option) => {
             option.addEventListener('click', function (e) {
-
-                mainGame.classList.add('hidding-magic');
+                mainGame.classList.add('show-main');
+                result.classList.add('show-result');
 
                 const user = e.target.id;
                 const comp = computer[Math.floor(Math.random() * computer.length)];
-                console.log(`user choice: ${user}`);
-                console.log(`computer choice: ${comp}`);
+
+                console.log(`user: ${user} ######## computer: ${comp}`);
+
+                playerChoose.classList.add(`${user}-magic`);
+                playerContainer.classList.add('player-magic-init');
+                computerChoose.classList.add(`${comp}-magic`);
+                computerContainer.classList.add('computer-magic-init');
+                announce.classList.add('announcement-magic');
 
                 if (user === comp) {
                     winner.textContent = "It's a draw!";
@@ -77,12 +97,33 @@ const callMeSupreme = () => {
                     scoreElement.textContent = score;
                 }
             });
-        });
+        });    
+        
+        const reset = () => {
+            resetBtn.addEventListener('click', function () {
+                winner.textContent = "";
+                //    scoreElement.textContent = 0;
+                mainGame.classList.remove('show-main');
+                result.classList.remove('show-result');
+
+                for (let rm of computer) {
+                    playerChoose.classList.remove(`${rm}-magic`);
+                    console.log(rm);
+                    computerChoose.classList.remove(`${rm}-magic`);
+                }
+                announce.classList.remove('announcement-magic');
+                playerContainer.classList.remove('player-magic-init');
+                computerContainer.classList.remove('computer-magic-init');
+
+            });
+        }
+        reset();
     }
 
     /*Calling inner functions*/
     rules();
     mainGame();
+
 }
 
 callMeSupreme();
